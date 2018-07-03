663cd47fa01ecd156f25b084ad8266bd4fb18a32
Kang In Park <kanginpark@Kangs-MacBook-Air.local>
2018-07-04 03:25:59 +0800

    txt to env

--- willabot.py
+++ willabot.py
@@ -1,9 +1,10 @@
 # https://github.com/Rapptz/discord.py/blob/async/examples/reply.py
 import discord
 from discord.ext import commands
+require('dotenv').config()
 
 
-with open('token.txt', 'r') as f:
+with open('token.env', 'r') as f:
     TOKEN = f.readline()
 print(TOKEN)
 
