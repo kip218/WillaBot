dd3f420a303e6e83cba7d93c062a3c5cd9766d35
Kang In Park <kanginpark@Kangs-MacBook-Air.local>
2018-07-04 20:08:21 +0800

    added command uptime

--- willabot.py
+++ willabot.py
@@ -1,4 +1,5 @@
 # https://github.com/Rapptz/discord.py/blob/async/examples/reply.py
+from datetime import datetime
 import discord
 from discord.ext import commands
 prefix = 'w.'
@@ -7,32 +8,18 @@ from settings import token
 
 
 commands_lst = ['ping', 'hello', 'help', 'echo', 'invite']
-help_msg = "***WillaBot Commands***\nThe prefix for the WillaBot is `w.`\n\n**w.help**\n~~You're looking right at it c:~~\n**w.invite**\nInvite link for WillaBot"
+help_msg = "***WillaBot Commands***\nThe prefix for the WillaBot is `w.`\n\n**w.help**\n~~You're looking right at it c:~~\n"
+
+bot.launch_time = datetime.utcnow()
 
 
-
-# @bot.event
-# async def on_message(message):
-#     if message.content.startswith('w.') and not message.author.bot:
-#         msg_full = message.content
-#         space_ind = msg_full.find(' ')
-#         if space_ind == -1:
-#             space_ind = len(msg_full)
-#         command = msg_full[2:space_ind]
-#         msg_rest = msg_full[space_ind:]
-
-
-#             elif command == 'invite' and msg_rest == '':
-#                 await message.channel.send('**Invite link for WillaBot:**\nhttps://discordapp.com/api/oauth2/authorize?client_id=463398601553346581&permissions=0&scope=bot')
-
-#         else:
-#             await message.channel.send("Oops, wrong command! Try 'w.help' for a list of commands!")
 
 #     #elif client.user.mentioned_in(message) and not message.author.bot:
 #     #   await message.channel.send("Please don't ping me {0.author.mention}, I'm a busy bot. Try 'w.help' instead.".format(message))
 
 #     elif message.content.lower() in ['what are you', 'what r u', 'wat are u', 'wat r you', 'what r you', 'what are u', 'wat are you', 'wat r u'] and not message.author.bot:
 #         await message.channel.send("AN IDIOT SANDWICH :bread::sob::bread:")
+
 
 @bot.command()
 async def hello(ctx):
@@ -54,6 +41,18 @@ async def ping(ctx):
 
 
 @bot.command()
+async def uptime(ctx):
+    '''
+    Uptime of WillaBot
+    '''
+    delta_uptime = datetime.utcnow() - bot.launch_time
+    hours, remainder = divmod(int(delta_uptime.total_seconds()), 3600)
+    minutes, seconds = divmod(remainder, 60)
+    days, hours = divmod(hours, 24)
+    await ctx.send(f"{days}d, {hours}h, {minutes}m, {seconds}s")
+
+
+@bot.command()
 async def echo(ctx, *, content:str):
     '''
     Makes WillaBot repeat message.
@@ -68,6 +67,7 @@ async def invite(ctx):
     '''
     await ctx.send('**Invite link for WillaBot:**\nhttps://discordapp.com/api/oauth2/authorize?client_id=463398601553346581&permissions=0&scope=bot')
 
+
 @bot.event
 async def on_ready():
     print(discord.__version__)
@@ -75,5 +75,8 @@ async def on_ready():
     #print(client.user.name)
     #print(client.user.id)
     print('------')
+    game = discord.Game("w.help")
+    await bot.change_presence(status=discord.Status.idle, activity=game)
+
 
 bot.run(token)
