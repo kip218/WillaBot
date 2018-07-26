90e7d1d40f56c959878517bcc5fd4e608e9d24ac
Kang In Park <kanginpark@Kangs-MacBook-Air.local>
2018-07-27 00:17:02 +0800

    user database

--- run.py
+++ run.py
@@ -54,12 +54,14 @@ async def on_message(message):
             await message.channel.send("¯\\_(ツ)_/¯")
 
         # add user to database
-        # conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-        # c = conn.cursor()
-        # c.execute(""" INSERT INTO users (ID, xp, balance)
-        #             VALUES (%s, %s, %s)
-        #             ON CONFLICT (ID)
-        #             DO NOTHING;""", (message.author.id, 0, 0))
+        conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+        c = conn.cursor()
+        c.execute(""" INSERT INTO users (ID, xp, balance)
+                    VALUES (%s, %s, %s)
+                    ON CONFLICT (ID)
+                    DO NOTHING;""", (message.author.id, 0, 0))
+        conn.commit()
+        conn.close()
 
     await bot.process_commands(message)
 
