cbca38c446552a67b122fbb052ad6bb943d519aa
Kang In Park <kip218@nyu.edu>
2018-07-31 02:42:07 +0800

    w.b info

--- cogs/Brawlhalla.py
+++ cogs/Brawlhalla.py
@@ -21,11 +21,31 @@ class Brawlhalla:
         '''
 
     @b.command()
-    async def test(self, ctx):
+    async def info(self, ctx, legend, skin: str='base', color: str='Classic_Colors'):
         '''
-        test
+        Gives info of a Brawlhalla legend, skin, color.
         '''
-        await ctx.send(file=discord.File("http://s3.amazonaws.com/willabot-assets/images/legends/ADA_Atlantean_Ada_Black.png"))
+        def format(string):
+            string_lst = string.split("-")
+            for substring_index in range(len(string_lst)):
+                substring = string_lst[substring_index]
+                substring = substring[0].upper() + substring[1:].lower()
+                string_lst[substring_index] = substring
+            res = "_".join(string_lst)
+            return res
+
+        legend = legend.upper()
+        if skin != "base":
+            skin = format(skin)
+        if color != "Classic_Colors":
+            color = format(color)
+        img_url_lst = [legend, skin, color]
+        img_url = '_'.join(img_url_lst)
+        print(img_url)
+        embed = discord.Embed()
+        embed.set_image(url="https://s3.amazonaws.com/willabot-assets/images/legends/" + img_url + ".png")
+        await ctx.send(embed=embed)
+
 
 def setup(bot):
     bot.add_cog(Brawlhalla(bot))
