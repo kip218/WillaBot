0038492f680e7b7d5dcdc9589f6aa1a68d26070c
Kang In Park <kip218@nyu.edu>
2018-09-04 00:46:24 -0400

    added w.weather

--- cogs/Fun.py
+++ cogs/Fun.py
@@ -1,9 +1,11 @@
+import discord
 from discord.ext import commands
 import os
 import requests
 
 DATABASE_URL = os.environ['DATABASE_URL']
 mashape_key = os.environ['mashape_key']
+weather_key = os.environ['weather_key']
 
 
 class Fun:
@@ -41,6 +43,54 @@ class Fun:
         quote = response['quote']
         author = response['author']
         await ctx.send(f"*\"{quote}\"*\n- {author}")
+
+    @commands.cooldown(rate=10, per=60, type=commands.BucketType.user)
+    @commands.command()
+    async def weather(self, ctx, *, city: str=None):
+        '''
+        Gets the weather forecast for a city.
+        w.weather <city>
+
+        If you wish to specify the country, add the 2-letter country code after the city.
+        eg) "w.weather Seoul, KR"
+        '''
+        if city is None:
+            await ctx.send("You must specify a city!")
+            return
+
+        try:
+            response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city}&APPID={weather_key}&units=metric").json()
+            main = response['weather'][0]['main']
+            description = response['weather'][0]['description']
+            weather_icon_code = response['weather'][0]['icon']
+            weather_icon_url = f"http://openweathermap.org/img/w/{weather_icon_code}.png"
+            temp = response['main']['temp']
+            humidity = response['main']['humidity']
+            temp_min = response['main']['temp_min']
+            temp_max = response['main']['temp_max']
+            country = response['sys']['country']
+            city = response['name']
+        except:
+            await ctx.send("City not found.")
+            return
+
+        embed = discord.Embed(title=f"Weather Forecast for {city}, {country}", description=f"**{main}**\n{description}", color=0x48d1cc)
+        embed.set_thumbnail(url=weather_icon_url)
+        embed.add_field(name="Temperature (°C)", value=f"{temp} ({temp_min} - {temp_max})")
+        embed.add_field(name="Humidity", value=humidity)
+        await ctx.send(embed=embed)
+
+    @weather.error
+    async def weather_error(self, ctx, error):
+        if isinstance(error, commands.CommandOnCooldown):
+            error_msg = str(error)
+            T_ind = error_msg.find("T")
+            error_msg = error_msg[T_ind:]
+            user = ctx.message.author
+            await ctx.send("Slow down " + user.mention + "! The command is on cooldown! " + error_msg + ".")
+        else:
+            await ctx.send("Unknown error. Please tell Willa.")
+            print(error)
 
 
 def setup(bot):
